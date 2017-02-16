7abe6833775c974b83a835c995a94d4bcdc2210d
hply <wangjd@go-goal.com>
2017-02-16 15:21:25 +0800

    添加适配器，提取主页面适配器

--- app/src/main/java/com/gogoal/app/base/BaseFragment.java
+++ app/src/main/java/com/gogoal/app/base/BaseFragment.java
@@ -3,6 +3,10 @@ package com.gogoal.app.base;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,6 +50,30 @@ public abstract class BaseFragment extends Fragment implements IBase {
         return fragmentTitle;
     }
 
+    /**
+     * 初始化垂直列表的RecycleView
+     * @param recyclerView:初始化对象;
+     * @param dividerId:分割线对象 : 0时为默认一条直线;int值 shape资源；null(不要分割线)
+     */
+    public void initRecycleView(RecyclerView recyclerView, Integer dividerId) {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        if (dividerId != null) {
+            if (dividerId != 0x00) {
+                try {
+                    DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL);
+                    itemDecoration.setDrawable(ContextCompat.getDrawable(getContext(), dividerId));//R.drawable.shape_divider
+                    recyclerView.addItemDecoration(itemDecoration);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("initRecycleView(RecyclerView,Integer)第二个参数必须是一个分割线shape资源或者填0或者null");
+                }
+            } else {
+                DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL);
+                recyclerView.addItemDecoration(itemDecoration);
+            }
+        }
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+    }
 
     @Override
     public void onDestroyView() {
--- app/src/main/java/com/gogoal/app/fragment/MessageFragment.java
+++ app/src/main/java/com/gogoal/app/fragment/MessageFragment.java
@@ -2,14 +2,20 @@ package com.gogoal.app.fragment;
 
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 
 import com.gogoal.app.R;
 import com.gogoal.app.base.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * 消息
  */
 public class MessageFragment extends BaseFragment {
+
+    @BindView(R.id.recycleView)
+    RecyclerView recycleView;
 
     public MessageFragment() {
     }
@@ -22,5 +28,9 @@ public class MessageFragment extends BaseFragment {
     @Override
     public void doBusiness(Context mContext) {
         setFragmentTitle("消息");
+        initRecycleView(recycleView,0);
     }
+
+
+
 }
