a372cc8478b96c5c02214f70fec829fb4908727f
Dave <daiwei@go-goal.com>
2017-02-17 13:54:53 +0800

    集成阿里播放器

---
+++ app/src/main/java/com/gogoal/app/common/PlayerUtils/StatusListener.java
@@ -0,0 +1,10 @@
+package com.gogoal.app.common.PlayerUtils;
+
+/**
+ * Created by dave.
+ * Date: 2017/2/17.
+ * Desc: description
+ */
+public interface StatusListener {
+    int notifyStatus(int status);
+}
