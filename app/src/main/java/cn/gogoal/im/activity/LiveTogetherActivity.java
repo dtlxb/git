fad94246cbbb6f8ca381b043ae4b03f895340a7a
Dave <daiwei@go-goal.com>
2017-03-21 14:33:04 +0800

    新建直播推流页面

---
+++ app/src/main/java/cn/gogoal/im/activity/LiveTogetherActivity.java
@@ -0,0 +1,22 @@
+package cn.gogoal.im.activity;
+
+import android.content.Context;
+
+import cn.gogoal.im.R;
+import cn.gogoal.im.base.BaseActivity;
+
+/*
+* 直播页面
+* */
+public class LiveTogetherActivity extends BaseActivity {
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_live_together;
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+}
