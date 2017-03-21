e553d47c808d0a522a8689809f2215b966ca4a57
Dave <daiwei@go-goal.com>
2017-03-21 14:56:35 +0800

    fix

--- app/src/main/java/cn/gogoal/im/activity/LiveTogetherActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/LiveTogetherActivity.java
@@ -1,7 +1,11 @@
 package cn.gogoal.im.activity;
 
+import android.Manifest;
 import android.content.Context;
+import android.os.Build;
+import android.view.SurfaceView;
 
+import butterknife.BindView;
 import cn.gogoal.im.R;
 import cn.gogoal.im.base.BaseActivity;
 
@@ -10,6 +14,18 @@ import cn.gogoal.im.base.BaseActivity;
 * */
 public class LiveTogetherActivity extends BaseActivity {
 
+    @BindView(R.id.camera_surface)
+    SurfaceView surfaceView;
+
+    //权限方面
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private final int PERMISSION_DELAY = 100;
+    private boolean mHasPermission = false;
+    private static final String[] permissionManifest = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+    };
+
     @Override
     public int bindLayout() {
         return R.layout.activity_live_together;
@@ -17,6 +33,12 @@ public class LiveTogetherActivity extends BaseActivity {
 
     @Override
     public void doBusiness(Context mContext) {
+        setImmersive(true);
 
+        if (Build.VERSION.SDK_INT >= 23) {
+
+        } else {
+            mHasPermission = true;
+        }
     }
 }
