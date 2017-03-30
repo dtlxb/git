18405e4afc55c701fc67dcdc689848e0e5423766
hply <wangjd@go-goal.com>
2017-03-30 19:59:27 +0800

    添加空数据是展示页面

--- app/src/main/java/cn/gogoal/im/fragment/SearchPersionFragment.java
+++ app/src/main/java/cn/gogoal/im/fragment/SearchPersionFragment.java
@@ -3,18 +3,25 @@ package cn.gogoal.im.fragment;
 import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.alibaba.fastjson.JSONObject;
 import com.socks.library.KLog;
 
 import org.simple.eventbus.Subscriber;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
 import cn.gogoal.im.R;
+import cn.gogoal.im.adapter.recycleviewAdapterHelper.CommonAdapter;
+import cn.gogoal.im.adapter.recycleviewAdapterHelper.base.ViewHolder;
+import cn.gogoal.im.adapter.recycleviewAdapterHelper.wrapper.EmptyWrapper;
 import cn.gogoal.im.base.BaseFragment;
+import cn.gogoal.im.bean.ContactBean;
 import cn.gogoal.im.common.GGOKHTTP.GGOKHTTP;
 import cn.gogoal.im.common.UIHelper;
 import cn.gogoal.im.common.UserUtils;
@@ -30,6 +37,8 @@ public class SearchPersionFragment extends BaseFragment {
 
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
+    private ArrayList<ContactBean> searchResultList;
+    private EmptyWrapper wrapper;
 
     @Override
     public int bindLayout() {
@@ -40,6 +49,16 @@ public class SearchPersionFragment extends BaseFragment {
     public void doBusiness(Context mContext) {
         recyclerView.addItemDecoration(new NormalItemDecoration(mContext));
         recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+
+        searchResultList = new ArrayList<>();
+
+        SearchPersionResultAdapter adapter = new SearchPersionResultAdapter(searchResultList);
+
+        wrapper = new EmptyWrapper(adapter);
+
+        wrapper.setEmptyView(UIHelper.getEmptyView(mContext));
+
+        recyclerView.setAdapter(wrapper);
     }
 
     @Subscriber(tag = "SEARCH_PERSION_TAG")
@@ -47,16 +66,24 @@ public class SearchPersionFragment extends BaseFragment {
         KLog.e(keyword);
         final Map<String, String> param = new HashMap<>();
         param.put("token", UserUtils.getToken());
-        param.put("keyword",keyword);
-        KLog.e("token="+UserUtils.getToken()+"&keyword="+keyword);
+        param.put("keyword", keyword);
+        KLog.e("token=" + UserUtils.getToken() + "&keyword=" + keyword);
         GGOKHTTP.GGHttpInterface ggHttpInterface = new GGOKHTTP.GGHttpInterface() {
             @Override
             public void onSuccess(String responseInfo) {
                 KLog.e(responseInfo);
                 if (JSONObject.parseObject(responseInfo).getIntValue("code") == 0) {
-                    parseAccount(responseInfo);
+                    JSONObject jsonObject = JSONObject.parseObject(responseInfo);
+                    List<ContactBean> resultList = JSONObject.parseArray(jsonObject.getJSONArray("data")
+                            .toJSONString(), ContactBean.class);
+
+                    searchResultList.addAll(resultList);
+                    wrapper.notifyDataSetChanged();
+
+                } else if (JSONObject.parseObject(responseInfo).getIntValue("code") == 1001) {
+                    
                 } else {
-                    UIHelper.toastResponseError(getActivity(),responseInfo);
+                    UIHelper.toastResponseError(getActivity(), responseInfo);
                 }
             }
 
@@ -68,9 +95,30 @@ public class SearchPersionFragment extends BaseFragment {
         new GGOKHTTP(param, GGOKHTTP.SEARCH_FRIEND, ggHttpInterface).startGet();
     }
 
-    private void parseAccount(String responseInfo) {
-        JSONObject jsonObject = JSONObject.parseObject(responseInfo);
+    private class SearchPersionResultAdapter extends CommonAdapter<ContactBean> {
 
+        SearchPersionResultAdapter(List<ContactBean> datas) {
+            super(SearchPersionFragment.this.getContext(), R.layout.item_search_type_persion, datas);
+        }
+
+        @Override
+        protected void convert(ViewHolder holder, ContactBean data, int position) {
+            KLog.e(JSONObject.toJSONString(data));
+            holder.setText(R.id.item_tv_user_name, data.getNickname());
+            try {
+                holder.setImageUrl(R.id.item_user_avatar, (String) data.getAvatar());
+            } catch (Exception e) {
+                KLog.e(e.getMessage());
+            }
+            UIHelper.setRippBg(holder.itemView);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
     }
 
 }
