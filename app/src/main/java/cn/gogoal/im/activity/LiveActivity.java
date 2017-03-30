333d537b13831524772cd337f62f4b65457b5212
Dave <daiwei@go-goal.com>
2017-03-30 16:54:11 +0800

    fix

---
+++ app/src/main/java/cn/gogoal/im/activity/LiveActivity.java
@@ -0,0 +1,19 @@
+package cn.gogoal.im.activity;
+
+import android.content.Context;
+
+import cn.gogoal.im.R;
+import cn.gogoal.im.base.BaseActivity;
+
+public class LiveActivity extends BaseActivity {
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_live;
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+}
