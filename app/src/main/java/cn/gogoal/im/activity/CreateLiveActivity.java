bca81e2a32eda18ab97f4812aaf9d8f2e82ed31a
Dave <daiwei@go-goal.com>
2017-04-17 13:25:07 +0800

    添加新页面

---
+++ app/src/main/java/cn/gogoal/im/activity/CreateLiveActivity.java
@@ -0,0 +1,22 @@
+package cn.gogoal.im.activity;
+
+import android.content.Context;
+
+import cn.gogoal.im.R;
+import cn.gogoal.im.base.BaseActivity;
+
+/*
+* 创建直播
+* */
+public class CreateLiveActivity extends BaseActivity {
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_create_live;
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+}
