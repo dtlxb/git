6f71b338f7be96f81fd281c6b0295ace2e9f0468
hply <wangjd@go-goal.com>
2017-04-19 15:34:48 +0800

    修复7.0机型，启动相机crash的问题

--- app/src/main/java/cn/gogoal/im/activity/TakePhotoActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/TakePhotoActivity.java
@@ -37,7 +37,7 @@ public class TakePhotoActivity extends Activity{
 
         int limit = getIntent().getIntExtra("limit", 9);
 
-        int canCropSize=getIntent().getIntExtra("canCropSize",200);
+        int canCropSize=getIntent().getIntExtra("canCropSize",1000);
 
         boolean canCrop = getIntent().getBooleanExtra("canCrop", false);
 
@@ -84,8 +84,8 @@ public class TakePhotoActivity extends Activity{
         imagePicker.setSaveRectangle(true);                     //是否按矩形区域保存
         imagePicker.setSelectLimit(limit);                      //选中数量限制
         imagePicker.setStyle(CropImageView.Style.RECTANGLE);   //裁剪框的形状
-        imagePicker.setFocusWidth(canCropSize);                  //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
-        imagePicker.setFocusHeight(canCropSize);                 //裁剪框的高度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setFocusWidth(800);                  //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setFocusHeight(800);                 //裁剪框的高度。单位像素（圆形自动取宽高最小值）
         imagePicker.setOutPutX(canCropSize);                           //保存文件的宽度。单位像素
         imagePicker.setOutPutY(canCropSize);                           //保存文件的高度。单位像素
         imagePicker.setMultiMode(limit!=1);
