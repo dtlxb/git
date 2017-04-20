bd9ef471253315c311ce4e62d96ffbd6dafe57d8
huangxx <huangxx@go-goal.com>
2017-04-20 09:56:13 +0800

    拿照片

--- app/src/main/java/cn/gogoal/im/activity/EditPersonInfoActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/EditPersonInfoActivity.java
@@ -1,5 +1,6 @@
 package cn.gogoal.im.activity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
@@ -9,21 +10,33 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hply.imagepicker.ITakePhoto;
 import com.socks.library.KLog;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import cn.gogoal.im.R;
+import cn.gogoal.im.base.AppManager;
 import cn.gogoal.im.base.BaseActivity;
+import cn.gogoal.im.common.DialogHelp;
+import cn.gogoal.im.common.DownloadCallBack;
+import cn.gogoal.im.common.DownloadUtils;
 import cn.gogoal.im.common.GGOKHTTP.GGOKHTTP;
 import cn.gogoal.im.common.ImageUtils.ImageDisplay;
+import cn.gogoal.im.common.ImageUtils.ImageTakeUtils;
+import cn.gogoal.im.common.UFileUpload;
 import cn.gogoal.im.common.UIHelper;
 import cn.gogoal.im.common.UserUtils;
 import cn.gogoal.im.ui.view.SelectorButton;
 import cn.gogoal.im.ui.view.XTitle;
+import cn.gogoal.im.ui.widget.BottomSheetListDialog;
 
 /**
  * Created by huangxx on 2017/4/18.
@@ -119,6 +132,97 @@ public class EditPersonInfoActivity extends BaseActivity {
         rigisterView.setTextColor(getResColor(R.color.colorPrimary));
     }
 
+    //上传头像到ufile
+    private void uploadAvatar(File chooseAvatar) {
+        //弹个窗
+        final ProgressDialog waitDialog =
+                DialogHelp.getWaitDialog(getActivity(), "上传中...");
+        waitDialog.setCancelable(false);
+        waitDialog.show();
+
+        //上传
+        UFileUpload.getInstance().upload(chooseAvatar,
+                UFileUpload.Type.IMAGE,
+                new UFileUpload.UploadListener() {
+                    @Override
+                    public void onUploading(int progress) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(String onlineUri) {
+                        waitDialog.dismiss();
+                        UIHelper.toast(getActivity(), "修改成功");
+                        AppManager.getInstance().sendMessage("updata_cache_avatar",
+                                onlineUri);
+                        KLog.e("ufile图片外链::" + onlineUri);
+//                                                    http://hackfile.ufile.ucloud.cn/GoGoal_3E21A216416826E307F2805796BE0C55.jpg@1000x1000
+                        UserUtils.updataLocalUserInfo("simple_avatar", onlineUri);
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("avatar", onlineUri);
+                        UserUtils.updataNetUserInfo(map, new UserUtils.UpdataListener() {
+                            @Override
+                            public void success(String responce) {
+                                KLog.e(responce);
+                            }
+
+                            @Override
+                            public void failed(String errorMsg) {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailed() {
+                        waitDialog.dismiss();
+                        UIHelper.toast(getActivity(), "修改出错！");
+                    }
+                });
+    }
+
+    private void openPicture() {
+        ImageTakeUtils.getInstance().takePhoto(getActivity(), 1, true, new ITakePhoto() {
+            @Override
+            public void success(List<String> uriPaths, boolean isOriginalPic) {
+                if (uriPaths != null && (!uriPaths.isEmpty())) {
+                    File chooseAvatar = new File(uriPaths.get(0));
+                    ImageDisplay.loadNetAvatar(getActivity(),uriPaths.get(0),imagePersonHeadpic);
+                    uploadAvatar(chooseAvatar);
+                }
+            }
+
+            @Override
+            public void error() {
+
+            }
+        });
+    }
+
+    /**
+     * 头像预览 弹窗
+     */
+    private void editDialogClick() {
+        DialogHelp.getBottomSheetListDialog(getActivity(), new ArrayList<>(Arrays.asList(
+                new String[]{"拍照", "从手机相册中选择"})), new BottomSheetListDialog.DialogItemClick() {
+            @Override
+            public void onItemClick(BottomSheetListDialog dialog, TextView view, int position) {
+                dialog.dismiss();
+                switch (position) {
+                    case 0:
+                        //打开相机
+                        //openCamera();
+
+                        break;
+                    case 1:
+                        //从手机相册中选择
+                        openPicture();
+                        break;
+                }
+            }
+        });
+    }
+
     @OnClick({R.id.login_cofirm, R.id.layout_person_headpic})
     void function(View view) {
         switch (view.getId()) {
@@ -126,6 +230,10 @@ public class EditPersonInfoActivity extends BaseActivity {
                 editPersonInfos();
                 break;
             case R.id.layout_person_headpic:
+                /*Intent intent = new Intent(getActivity(), ImageDetailActivity.class);
+                intent.putExtra("account_Id", UserUtils.getUserAccountId());
+                startActivity(intent);*/
+                editDialogClick();
                 break;
             default:
                 break;
