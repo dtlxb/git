4c9a192d94fb110fb03d43b2c6ed9ecea154956a
Dave <daiwei@go-goal.com>
2017-04-21 09:58:08 +0800

    添加发起直播入口

--- app/src/main/java/cn/gogoal/im/activity/EditMyInfoActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/EditMyInfoActivity.java
@@ -7,15 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.alibaba.fastjson.JSONObject;
-import com.socks.library.KLog;
-
 import org.simple.eventbus.Subscriber;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import butterknife.BindArray;
 import butterknife.BindView;
@@ -24,8 +19,6 @@ import cn.gogoal.im.adapter.baseAdapter.BaseMultiItemQuickAdapter;
 import cn.gogoal.im.adapter.baseAdapter.BaseViewHolder;
 import cn.gogoal.im.base.BaseActivity;
 import cn.gogoal.im.bean.UserDetailInfo;
-import cn.gogoal.im.common.DialogHelp;
-import cn.gogoal.im.common.GGOKHTTP.GGOKHTTP;
 import cn.gogoal.im.common.ImageUtils.ImageDisplay;
 import cn.gogoal.im.common.UIHelper;
 import cn.gogoal.im.common.UserUtils;
@@ -137,7 +130,6 @@ public class EditMyInfoActivity extends BaseActivity {
                                 case 5://职位
                                     break;
                                 case 6://工作地区
-                                    getUserValid();
                                     break;
                             }
                         }
@@ -152,48 +144,5 @@ public class EditMyInfoActivity extends BaseActivity {
         editInfos.remove(0);
         editInfos.add(0, new UserDetailInfo(UserDetailInfo.HEAD, newAvatarUrl));
         myInfoAdapter.notifyItemChanged(0);
-    }
-
-    /*
-    * 能否发起直播
-    * */
-    private void getUserValid() {
-
-        Map<String, String> param = new HashMap<>();
-        param.put("token", UserUtils.getToken());
-
-        GGOKHTTP.GGHttpInterface ggHttpInterface = new GGOKHTTP.GGHttpInterface() {
-            @Override
-            public void onSuccess(String responseInfo) {
-                KLog.json(responseInfo);
-                JSONObject object = JSONObject.parseObject(responseInfo);
-                if (object.getIntValue("code") == 0) {
-                    JSONObject data = object.getJSONObject("data");
-                    if (data.getIntValue("code") == 1) {
-                        if (data.getString("live_id") != null) {
-                            Intent intent = new Intent(getContext(), LiveActivity.class);
-                            intent.putExtra("live_id", data.getString("live_id"));
-                            startActivity(intent);
-                        } else {
-                            startActivity(new Intent(getActivity(), CreateLiveActivity.class));
-                        }
-                    } else {
-                        DialogHelp.getMessageDialog(getContext(), "您暂时没有权限直播，请联系客服申请！").show();
-                    }
-                } else {
-                    UIHelper.toast(getContext(), R.string.net_erro_hint);
-                }
-            }
-
-            @Override
-            public void onFailure(String msg) {
-                UIHelper.toast(getContext(), R.string.net_erro_hint);
-            }
-        };
-        new GGOKHTTP(param, GGOKHTTP.VIDEO_MOBILE, ggHttpInterface).startGet();
-    }
-
-    public EditMyInfoActivity getContext() {
-        return EditMyInfoActivity.this;
     }
 }
