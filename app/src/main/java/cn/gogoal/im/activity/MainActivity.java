6e5de8831d6f18de2701811819a2c045b3fc99c8
hply <wangjd@go-goal.com>
2017-04-26 22:06:14 +0800

    彻底解决小红点问题

--- app/src/main/java/cn/gogoal/im/activity/MainActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.AnimationUtils;
@@ -38,7 +39,7 @@ import cn.gogoal.im.fragment.MessageFragment;
 import cn.gogoal.im.fragment.MineFragment;
 import cn.gogoal.im.fragment.MyStockFragment;
 import cn.gogoal.im.fragment.SocialContactFragment;
-import hply.com.niugu.autofixtext.AutofitTextView;
+import cn.gogoal.im.ui.Badge.BadgeView;
 
 public class MainActivity extends BaseActivity {
 
@@ -55,8 +56,6 @@ public class MainActivity extends BaseActivity {
 
     private MyStockFragment myStockFragment;
 
-    private AutofitTextView[] badgeView;
-
     @Override
     public int bindLayout() {
         return R.layout.activity_main;
@@ -65,7 +64,7 @@ public class MainActivity extends BaseActivity {
     @BindArray(R.array.main_tab)
     String[] mainTabArray;
 
-
+    private BadgeView badge;
     @Override
     public void doBusiness(Context mContext) {
 
@@ -109,13 +108,10 @@ public class MainActivity extends BaseActivity {
         vpMain.setOffscreenPageLimit(mainTabArray.length - 1);
         tabMain.setupWithViewPager(vpMain);
 
-        badgeView = new AutofitTextView[mainTabArray.length];
-
         for (int i = 0; i < mainTabArray.length; i++) {
             TabLayout.Tab tab = tabMain.getTabAt(i);
             if (tab != null) {
                 tab.setCustomView(tabAdapter.getTabView(i));
-                badgeView[i] = (AutofitTextView) tabAdapter.getTabView(i).findViewById(R.id.count_tv);
             }
         }
 
@@ -225,6 +221,7 @@ public class MainActivity extends BaseActivity {
 
     @Subscriber(tag = "correct_allmessage_count")
     public void setBadgeViewNum(BaseMessage<Integer> message) {
+        initBadge();
 
         int index = message.getOthers().get("index");
         int num = message.getOthers().get("number");
@@ -232,12 +229,23 @@ public class MainActivity extends BaseActivity {
         KLog.e("index=="+index+";num=="+num);
 
         if (index >= 0 && index < mainTabArray.length) {
-            badgeView[index].setText(num > 99 ? "99+" : String.valueOf(num));
+            badge.bindTarget(tabMain.getTabAt(index).getCustomView());
         }
         if (num == 0) {
-            badgeView[index].setVisibility(View.GONE);
+            badge.hide(false);
         } else {
-            badgeView[index].setVisibility(View.VISIBLE);
-        } ;
+            badge.setBadgeNumber(num);
+        };
+    }
+
+    private void initBadge(){
+        badge=new BadgeView(MainActivity.this);
+
+        badge.setGravityOffset(0,0,true);
+        badge.setShowShadow(false);
+
+        badge.setBadgeGravity(Gravity.TOP | Gravity.END);
+        badge.setBadgeTextSize(10, true);
+        badge.setBadgePadding(4, true);
     }
 }
