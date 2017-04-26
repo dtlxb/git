08ab10a1537161a288b93c7dda0cc030aebfc2ce
huangxx <huangxx@go-goal.com>
2017-04-26 15:19:25 +0800

    群名片

--- app/src/main/java/cn/gogoal/im/activity/SquareCardActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/SquareCardActivity.java
@@ -1,8 +1,22 @@
 package cn.gogoal.im.activity;
 
+import android.content.Context;
+
+import cn.gogoal.im.R;
+import cn.gogoal.im.base.BaseActivity;
+
 /**
  * Created by huangxx on 2017/4/26.
  */
 
-public class SquareCardActivity {
+public class SquareCardActivity extends BaseActivity{
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_square_card;
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
 }
