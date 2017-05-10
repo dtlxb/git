e32dd0ba6b85b97c404d0389b317302632c6433f
Dave <daiwei@go-goal.com>
2017-05-10 10:45:58 +0800

    直播创建默认图

--- app/src/main/java/cn/gogoal/im/activity/CreateLiveActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/CreateLiveActivity.java
@@ -159,7 +159,9 @@ public class CreateLiveActivity extends BaseActivity {
         Map<String, String> param = new HashMap<>();
         param.put("token", UserUtils.getToken());
         param.put("live_title", title);
-        param.put("live_large_img", liveLargeImg);
+        if (liveLargeImg != null) {
+            param.put("live_large_img", liveLargeImg);
+        }
 
         GGOKHTTP.GGHttpInterface ggHttpInterface = new GGOKHTTP.GGHttpInterface() {
             @Override
