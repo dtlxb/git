c7f76c031111d55c3a3040112577d08b70857aa8
Dave <daiwei@go-goal.com>
2017-05-12 16:07:11 +0800

    邀约完成

--- app/src/main/java/cn/gogoal/im/ui/dialog/InviteAuthDialog.java
+++ app/src/main/java/cn/gogoal/im/ui/dialog/InviteAuthDialog.java
@@ -1,5 +1,7 @@
 package cn.gogoal.im.ui.dialog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,8 +14,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import cn.gogoal.im.R;
+import cn.gogoal.im.activity.PlayerActivity;
+import cn.gogoal.im.activity.WatchLiveActivity;
 import cn.gogoal.im.common.AppDevice;
 import cn.gogoal.im.common.GGOKHTTP.GGOKHTTP;
+import cn.gogoal.im.common.SPTools;
 import cn.gogoal.im.common.UserUtils;
 import cn.gogoal.im.ui.dialog.base.BaseCentDailog;
 
@@ -80,7 +85,7 @@ public class InviteAuthDialog extends BaseCentDailog {
         textSure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validateInviteCode();
+                validateInviteCode(view.getContext());
             }
         });
     }
@@ -88,9 +93,9 @@ public class InviteAuthDialog extends BaseCentDailog {
     /*
     * 验证邀请码
     * */
-    private void validateInviteCode() {
+    private void validateInviteCode(final Context mContext) {
 
-        String invite_code = editInvite.getText().toString();
+        final String invite_code = editInvite.getText().toString();
 
         if (invite_code.equals("")) {
             textError.setVisibility(View.VISIBLE);
@@ -115,14 +120,38 @@ public class InviteAuthDialog extends BaseCentDailog {
                 JSONObject object = JSONObject.parseObject(responseInfo);
                 if (object.getIntValue("code") == 0) {
                     JSONObject data = object.getJSONObject("data");
-                    if (data.getIntValue("code") == 201) {
+                    if (data.getIntValue("code") == 101) {
+
+                        SPTools.saveString(invite_id, invite_code);
+
+                        InviteAuthDialog.this.dismiss();
+
+                        if (liveSource.equals("live")) {
+                            Intent intent = new Intent(mContext, WatchLiveActivity.class);
+                            intent.putExtra("live_id", invite_id);
+                            mContext.startActivity(intent);
+                        } else if (liveSource.equals("video")) {
+                            Intent intent = new Intent(mContext, PlayerActivity.class);
+                            intent.putExtra("live_id", invite_id);
+                            mContext.startActivity(intent);
+                        }
+                    } else if (data.getIntValue("code") == 202) {
+                        textError.setVisibility(View.VISIBLE);
+                        textError.setText("邀请码已被使用");
+                    } else {
+                        textError.setVisibility(View.VISIBLE);
                         textError.setText("邀请码输入错误");
                     }
+                } else {
+                    textError.setVisibility(View.VISIBLE);
+                    textError.setText("邀请码输入错误");
                 }
             }
 
             @Override
             public void onFailure(String msg) {
+                textError.setVisibility(View.VISIBLE);
+                textError.setText("邀请码输入错误");
             }
         };
         new GGOKHTTP(param, GGOKHTTP.VALIDATE_CODE, ggHttpInterface).startGet();
