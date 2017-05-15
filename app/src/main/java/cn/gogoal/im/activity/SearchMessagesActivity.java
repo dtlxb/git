beb0eebf8d362b3005d23d0b430528f5f497670e
huangxx <huangxx@go-goal.com>
2017-05-15 18:03:22 +0800

    搜索页面

---
+++ app/src/main/java/cn/gogoal/im/activity/SearchMessagesActivity.java
@@ -0,0 +1,10 @@
+package cn.gogoal.im.activity;
+
+import cn.gogoal.im.base.BaseActivity;
+
+/**
+ * Created by huangxx on 2017/5/15.
+ */
+
+public class SearchMessagesActivity extends BaseActivity {
+}
