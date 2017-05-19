95055661e646c1db48565c4e6d1833d1625dd310
huangxx <huangxx@go-goal.com>
2017-05-19 16:27:39 +0800

    群全局搜索

---
+++ app/src/main/java/cn/gogoal/im/adapter/SearchListAdapter.java
@@ -0,0 +1,10 @@
+package cn.gogoal.im.adapter;
+
+import cn.gogoal.im.adapter.baseAdapter.CommonAdapter;
+
+/**
+ * Created by huangxx on 2017/5/19.
+ */
+
+public class SearcnListAdapter extends CommonAdapter {
+}
