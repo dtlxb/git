247569da01944e171d201b9ebbce11c52401a525
huangxx <huangxx@go-goal.com>
2017-06-12 14:33:43 +0800

    柱线绘制完成

---
+++ app/src/main/java/cn/gogoal/im/activity/ChatDemoActivity.java
@@ -0,0 +1,10 @@
+package cn.gogoal.im.activity;
+
+import cn.gogoal.im.base.BaseActivity;
+
+/**
+ * Created by huangxx on 2017/6/9.
+ */
+
+public class chatDemoActivity extends BaseActivity {
+}
---
+++ app/src/main/java/cn/gogoal/im/ui/view/BarView.java
@@ -0,0 +1,8 @@
+package cn.gogoal.im.ui.view;
+
+/**
+ * Created by huangxx on 2017/6/9.
+ */
+
+public class BarView {
+}
