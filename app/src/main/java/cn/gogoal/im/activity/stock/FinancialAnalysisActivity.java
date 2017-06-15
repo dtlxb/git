6a9c841055c9a87f651df85e401b22a8c58d0082
Dave <daiwei@go-goal.com>
2017-06-15 10:23:23 +0800

    财务分析

--- app/src/main/java/cn/gogoal/im/activity/stock/FinancialAnalysisActivity.java
+++ app/src/main/java/cn/gogoal/im/activity/stock/FinancialAnalysisActivity.java
@@ -1,6 +1,7 @@
 package cn.gogoal.im.activity.stock;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSONArray;
@@ -13,8 +14,10 @@ import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import cn.gogoal.im.R;
 import cn.gogoal.im.adapter.AnalysisLeftAdapter;
+import cn.gogoal.im.adapter.AnalysisRightAdapter;
 import cn.gogoal.im.base.BaseActivity;
 import cn.gogoal.im.bean.ChartBean;
 import cn.gogoal.im.common.GGOKHTTP.GGOKHTTP;
@@ -22,7 +25,6 @@ import cn.gogoal.im.common.UIHelper;
 import cn.gogoal.im.common.copy.FtenUtils;
 import cn.gogoal.im.ui.copy.InnerListView;
 import cn.gogoal.im.ui.view.BarView;
-import cn.gogoal.im.ui.view.MySyncHorizontalScrollView;
 
 /**
  * Created by dave.
@@ -33,6 +35,10 @@ public class FinancialAnalysisActivity extends BaseActivity {
 
     @BindView(R.id.textFormName)
     TextView textFormName;
+    @BindView(R.id.chart_tab_one)
+    TextView chart_tab_one;
+    @BindView(R.id.chart_tab_two)
+    TextView chart_tab_two;
     @BindView(R.id.barAnalysisView)
     BarView barAnalysisView;
 
@@ -40,15 +46,19 @@ public class FinancialAnalysisActivity extends BaseActivity {
     InnerListView lsvLeft;
     @BindView(R.id.lsv_right)
     InnerListView lsvRight;
-    @BindView(R.id.rightHorscrollView)
-    MySyncHorizontalScrollView rightHorscrollView;
 
     private String stockCode;
     private String stockName;
     private String stype;
     private String type;
 
+    private JSONObject data;
+    private int chartTab = 0;
+
+    List<ChartBean> chartBeanList = new ArrayList<>();
+
     private AnalysisLeftAdapter leftAdapter;
+    private AnalysisRightAdapter rightAdapter;
 
     @Override
     public int bindLayout() {
@@ -91,8 +101,9 @@ public class FinancialAnalysisActivity extends BaseActivity {
                 KLog.e(responseInfo);
                 JSONObject object = JSONObject.parseObject(responseInfo);
                 if (object.getIntValue("code") == 0) {
-                    JSONObject data = object.getJSONObject("data");
-                    setBarViewData(data);
+                    data = object.getJSONObject("data");
+                    setBarViewData(data, chartTab);
+                    setRightListData(data);
                 }
             }
 
@@ -104,59 +115,79 @@ public class FinancialAnalysisActivity extends BaseActivity {
         new GGOKHTTP(param, GGOKHTTP.FINANCIAL_ANALYSIS, ggHttpInterface).startGet();
     }
 
+    @OnClick({R.id.chart_tab_one, R.id.chart_tab_two, R.id.radioBtn0, R.id.radioBtn1,
+            R.id.radioBtn2, R.id.radioBtn3, R.id.radioBtn4})
+    public void ChartTabClick(View v) {
+        switch (v.getId()) {
+            case R.id.chart_tab_one:
+                chart_tab_one.setEnabled(false);
+                chart_tab_two.setEnabled(true);
+                chart_tab_one.setBackgroundResource(R.drawable.chart_tab_shape_select_left);
+                chart_tab_one.setTextColor(getResColor(R.color.white));
+                chart_tab_two.setTextColor(getResColor(R.color.colorPrimary));
+                chart_tab_two.setBackgroundResource(0);
+
+                if (stype.equals("1")) {
+                    textFormName.setText(FtenUtils.title1[Integer.parseInt(type) - 1]);
+                } else {
+                    textFormName.setText(FtenUtils.title2[Integer.parseInt(type) - 1]);
+                }
+
+                chartTab = 0;
+                setBarViewData(data, chartTab);
+                break;
+            case R.id.chart_tab_two:
+                chart_tab_one.setEnabled(true);
+                chart_tab_two.setEnabled(false);
+                chart_tab_two.setBackgroundResource(R.drawable.chart_tab_shape_select_right);
+                chart_tab_two.setTextColor(getResColor(R.color.white));
+                chart_tab_one.setTextColor(getResColor(R.color.colorPrimary));
+                chart_tab_one.setBackgroundResource(0);
+
+                if (stype.equals("1")) {
+                    textFormName.setText(FtenUtils.title1_1[Integer.parseInt(type) - 1]);
+                } else {
+                    textFormName.setText(FtenUtils.title2_1[Integer.parseInt(type) - 1]);
+                }
+
+                chartTab = 1;
+                setBarViewData(data, chartTab);
+                break;
+            case R.id.radioBtn0:
+                getFinacialData("0");
+                break;
+            case R.id.radioBtn1:
+                getFinacialData("4");
+                break;
+            case R.id.radioBtn2:
+                getFinacialData("2");
+                break;
+            case R.id.radioBtn3:
+                getFinacialData("1");
+                break;
+            case R.id.radioBtn4:
+                getFinacialData("3");
+                break;
+        }
+    }
+
     /**
      * 设置柱状图数据
      */
-    private void setBarViewData(JSONObject data) {
+    private void setBarViewData(JSONObject data, int chartTab) {
         JSONArray retained_profits = null;
         String profits = null;
-        if (stype.equals("1")) {
-            switch (type) {
-                case "1":
-                    profits = "retained_profits10";
-                    break;
-                case "2":
-                    profits = "perbasic_eps20";
-                    break;
-                case "3":
-                    profits = "epopratio_profit35";
-                    break;
-                case "4":
-                    profits = "asset_liratio46";
-                    break;
-                case "5":
-                    profits = "debtflow_ratio53";
-                    break;
-                case "6":
-                    profits = "service_saveloan72";
-                    break;
-                case "7":
-                    profits = "business_totalrevenue_growthrate90";
-                    break;
+        if (chartTab == 0) {
+            if (stype.equals("1")) {
+                profits = FtenUtils.cotent1[Integer.parseInt(type) - 1];
+            } else {
+                profits = FtenUtils.cotent2[Integer.parseInt(type) - 1];
             }
         } else {
-            switch (type) {
-                case "1":
-                    profits = "ffajr_09";
-                    break;
-                case "2":
-                    profits = "ffajr_19";
-                    break;
-                case "3":
-                    profits = "ffajr_29";
-                    break;
-                case "4":
-                    profits = "ffajr_38";
-                    break;
-                case "5":
-                    profits = "ffajr_45";
-                    break;
-                case "6":
-                    profits = "ffajr_50";
-                    break;
-                case "7":
-                    profits = "ffajr_57";
-                    break;
+            if (stype.equals("1")) {
+                profits = FtenUtils.cotent1_1[Integer.parseInt(type) - 1];
+            } else {
+                profits = FtenUtils.cotent2_1[Integer.parseInt(type) - 1];
             }
         }
         retained_profits = data.getJSONArray(profits);
@@ -172,11 +203,10 @@ public class FinancialAnalysisActivity extends BaseActivity {
             dates.add(FtenUtils.getReportType(title.getString(i)));
         }
 
-        List<ChartBean> chartBeanList = new ArrayList<>();
+        chartBeanList.clear();
         for (int i = 0; i < title.size(); i++) {
             chartBeanList.add(new ChartBean(values.get(i), dates.get(i)));
         }
-
         barAnalysisView.setChartData(chartBeanList);
     }
 
@@ -243,6 +273,72 @@ public class FinancialAnalysisActivity extends BaseActivity {
 
         leftAdapter = new AnalysisLeftAdapter(getActivity(), titleList);
         lsvLeft.setAdapter(leftAdapter);
+    }
+
+    /**
+     * 设置右边列表数据
+     */
+    private void setRightListData(JSONObject data) {
+        ArrayList<JSONArray> contList = new ArrayList<>();
+        contList.add(data.getJSONArray("title"));
+
+        String[] stringList = null;
+        if (stype.equals("1")) {
+            switch (type) {
+                case "1":
+                    stringList = FtenUtils.profit1_1;
+                    break;
+                case "2":
+                    stringList = FtenUtils.share_eps1_1;
+                    break;
+                case "3":
+                    stringList = FtenUtils.ProfitRate1_1;
+                    break;
+                case "4":
+                    stringList = FtenUtils.debtratio1_1;
+                    break;
+                case "5":
+                    stringList = FtenUtils.sovency1_1;
+                    break;
+                case "6":
+                    stringList = FtenUtils.turnoverrate1_1;
+                    break;
+                case "7":
+                    stringList = FtenUtils.grows1_1;
+                    break;
+            }
+        } else {
+            switch (type) {
+                case "1":
+                    stringList = FtenUtils.profit2_1;
+                    break;
+                case "2":
+                    stringList = FtenUtils.share_eps2_1;
+                    break;
+                case "3":
+                    stringList = FtenUtils.ProfitRate2_1;
+                    break;
+                case "4":
+                    stringList = FtenUtils.debtratio2_1;
+                    break;
+                case "5":
+                    stringList = FtenUtils.sovency2_1;
+                    break;
+                case "6":
+                    stringList = FtenUtils.turnoverrate2_1;
+                    break;
+                case "7":
+                    stringList = FtenUtils.grows2;
+                    break;
+            }
+        }
+
+        for (int i = 0; i < stringList.length; i++) {
+            contList.add(data.getJSONArray(stringList[i]));
+        }
+
+        rightAdapter = new AnalysisRightAdapter(getContext(), contList);
+        lsvRight.setAdapter(rightAdapter);
     }
 
     private FinancialAnalysisActivity getContext() {
--- app/src/main/java/cn/gogoal/im/adapter/AnalysisRightAdapter.java
+++ app/src/main/java/cn/gogoal/im/adapter/AnalysisRightAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSONArray;
+
 import java.util.ArrayList;
 
 import cn.gogoal.im.R;
@@ -20,11 +22,13 @@ import cn.gogoal.im.adapter.copy.MyBaseAdapter;
  */
 public class AnalysisRightAdapter extends MyBaseAdapter {
 
-    private ArrayList<String> titleList;
+    private ArrayList<JSONArray> contList;
     private Context context;
 
-    public AnalysisRightAdapter(ArrayList list) {
+    public AnalysisRightAdapter(Context context, ArrayList list) {
         super(list);
+        this.context = context;
+        this.contList = list;
     }
 
     @Override
@@ -34,7 +38,11 @@ public class AnalysisRightAdapter extends MyBaseAdapter {
             convertView = LayoutInflater.from(context).inflate(R.layout.item_analysis_right, parent, false);
             holder = new ViewHolder();
             holder.linearAnalysis = (LinearLayout) convertView.findViewById(R.id.linearAnalysis);
-            holder.textTitle = (TextView) convertView.findViewById(R.id.textTitle);
+            holder.textReport1 = (TextView) convertView.findViewById(R.id.textReport1);
+            holder.textReport2 = (TextView) convertView.findViewById(R.id.textReport2);
+            holder.textReport3 = (TextView) convertView.findViewById(R.id.textReport3);
+            holder.textReport4 = (TextView) convertView.findViewById(R.id.textReport4);
+            holder.textReport5 = (TextView) convertView.findViewById(R.id.textReport5);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -46,13 +54,30 @@ public class AnalysisRightAdapter extends MyBaseAdapter {
             holder.linearAnalysis.setBackgroundColor(ContextCompat.getColor(context, R.color.bg_color_2));
         }
 
-        holder.textTitle.setText(titleList.get(position));
+        if (contList.get(position).size() == 0) {
+            holder.textReport1.setText("--");
+            holder.textReport2.setText("--");
+            holder.textReport3.setText("--");
+            holder.textReport4.setText("--");
+            holder.textReport5.setText("--");
+        } else {
+            holder.textReport1.setText(contList.get(position).getString(0) != null ?
+                    contList.get(position).getString(0) : "--");
+            holder.textReport2.setText(contList.get(position).getString(1) != null ?
+                    contList.get(position).getString(1) : "--");
+            holder.textReport3.setText(contList.get(position).getString(2) != null ?
+                    contList.get(position).getString(2) : "--");
+            holder.textReport4.setText(contList.get(position).getString(3) != null ?
+                    contList.get(position).getString(3) : "--");
+            holder.textReport5.setText(contList.get(position).getString(4) != null ?
+                    contList.get(position).getString(4) : "--");
+        }
 
         return convertView;
     }
 
     class ViewHolder {
         private LinearLayout linearAnalysis;
-        private TextView textTitle;
+        private TextView textReport1, textReport2, textReport3, textReport4, textReport5;
     }
 }
