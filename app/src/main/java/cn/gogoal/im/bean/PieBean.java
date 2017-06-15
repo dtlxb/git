88ea0cf8cf544897f44f3b3bac98c853d2da0e19
huangxx <huangxx@go-goal.com>
2017-06-15 15:18:15 +0800

    饼状图等

---
+++ app/src/main/java/cn/gogoal/im/bean/PieBean.java
@@ -0,0 +1,8 @@
+package cn.gogoal.im.bean;
+
+/**
+ * Created by huangxx on 2017/6/14.
+ */
+
+public class PieBean {
+}
---
+++ app/src/main/java/cn/gogoal/im/ui/view/MultiBarView.java
@@ -0,0 +1,10 @@
+package cn.gogoal.im.ui.view;
+
+import android.view.View;
+
+/**
+ * Created by huangxx on 2017/6/14.
+ */
+
+public class MultiBarView extends View {
+}
