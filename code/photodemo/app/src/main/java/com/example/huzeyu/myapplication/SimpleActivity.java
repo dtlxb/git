615dea0c721e53a6038c0d54100f0ef3be1f4132
16Huzeyu <923704879@qq.com>
2017-06-30 09:57:45 +0800

    photodemo1.0

---
+++ code/photodemo/app/src/main/java/com/example/huzeyu/myapplication/SimpleActivity.java
@@ -0,0 +1,53 @@
+package com.example.huzeyu.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.model.TImage;
+import com.jph.takephoto.model.TResult;
+
+import java.util.ArrayList;
+
+/**
+ * Created by huzeyu on 2017/6/29.
+ */
+
+public class SimpleActivity extends TakePhotoActivity {
+    private CustomHelper customHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View contentView= LayoutInflater.from(this).inflate(R.layout.common_layout,null);
+        setContentView(contentView);
+        customHelper=CustomHelper.of(contentView);
+    }
+
+    public void onClick(View view) {
+        customHelper.onClick(view,getTakePhoto());
+    }
+
+    @Override
+    public void takeCancel() {
+        super.takeCancel();
+    }
+
+    @Override
+    public void takeFail(TResult result, String msg) {
+        super.takeFail(result, msg);
+    }
+
+    @Override
+    public void takeSuccess(TResult result) {
+        super.takeSuccess(result);
+        showImg(result.getImages());
+    }
+
+    private void showImg(ArrayList<TImage> images) {
+        Intent intent=new Intent(this,ResultActivity.class);
+        intent.putExtra("images",images);
+        startActivity(intent);
+    }
+}
