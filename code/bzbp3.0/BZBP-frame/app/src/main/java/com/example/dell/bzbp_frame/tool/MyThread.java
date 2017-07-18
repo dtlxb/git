c53f8716e4bbfd8457df80b17226828161792c7f
16Huzeyu <923704879@qq.com>
2017-07-18 17:17:10 +0800

    no message

--- code/bzbp3.0/BZBP-frame/app/src/main/java/com/example/dell/bzbp_frame/tool/MyThread.java
+++ code/bzbp3.0/BZBP-frame/app/src/main/java/com/example/dell/bzbp_frame/tool/MyThread.java
@@ -8,6 +8,7 @@ package com.example.dell.bzbp_frame.tool;
 import com.example.dell.bzbp_frame.model.Comment;
 import com.example.dell.bzbp_frame.model.MyLatlng;
 import com.example.dell.bzbp_frame.model.Posto;
+import com.example.dell.bzbp_frame.model.Praise;
 import com.example.dell.bzbp_frame.model.Route;
 import com.example.dell.bzbp_frame.model.User;
 import com.google.gson.Gson;
@@ -33,6 +34,17 @@ public class MyThread extends Thread {
     private int what ;      //选择传输的方法
     private   String getUrl;
     private ArrayList<Posto> Postos ;
+    private  Praise praise;
+
+    public Praise getPraise() {
+        return praise;
+    }
+
+    public void setPraise(Praise praise) {
+        this.praise = praise;
+    }
+
+
 
 
 
@@ -155,6 +167,17 @@ public class MyThread extends Thread {
         if(what==6){
             Comments= getComments (getUrl, posto);
         }
+
+        if(what==7){
+            result= sendPraise (getUrl, praise);
+        }
+        if(what==8){
+
+            result= getPraiseNumber(getUrl, posto);
+            rid = Integer.parseInt(result);
+        }
+
+
     }
 
     public String sendHttpPost(String getUrl, User user) {
@@ -333,16 +356,13 @@ public class MyThread extends Thread {
                     postotmp.setLongitude(thisposto.getDouble("longitude"));
                     postotmp.setPid(Integer.parseInt(thisposto.getString("pid")));
                     postotmp.setImage(thisposto.getString("image"));
-                    postotmp.setBelong_rid(thisposto.getInt("belong_rid"));
-<<<<<<< HEAD
+
 
                      postotmp.setDate(thisposto.getLong("date"));
 
 
-=======
-                     postotmp.setDate(thisposto.getLong("date"));
-                    postotmp.setDate(thisposto.getLong("date"));
->>>>>>> 889a59730389d58cd59efd9ca3058d756c2a307a
+
+
                     Postos.add(postotmp);
                 }
 
@@ -651,4 +671,127 @@ public class MyThread extends Thread {
         }
         return null;
     }
+
+
+    public String sendPraise(String getUrl, Praise praise) {
+        HttpURLConnection urlConnection = null;
+        URL url = null;
+        try {
+            url = new URL(getUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();//打开http连接
+            urlConnection.setConnectTimeout(3000);//连接的超时时间
+            urlConnection.setUseCaches(false);//不使用缓存
+            //urlConnection.setFollowRedirects(false);是static函数，作用于所有的URLConnection对象。
+            urlConnection.setInstanceFollowRedirects(true);//是成员函数，仅作用于当前函数,设置这个连接是否可以被重定向
+            urlConnection.setReadTimeout(3000);//响应的超时时间
+            urlConnection.setDoInput(true);//设置这个连接是否可以写入数据
+            urlConnection.setDoOutput(true);//设置这个连接是否可以输出数据
+            urlConnection.setRequestMethod("POST");//设置请求的方式
+            urlConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");//设置消息的类型
+            urlConnection.connect();// 连接，从上述至此的配置必须要在connect之前完成，实际上它只是建立了一个与服务器的TCP连接
+            Gson gson = new Gson();
+            String jsonstr = gson.toJson(praise);
+            LogUtil.d("1",jsonstr);
+            //------------字符流写入数据------------
+            OutputStream out = urlConnection.getOutputStream();//输出流，用来发送请求，http请求实际上直到这个函数里面才正式发送出去
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));//创建字符流对象并用高效缓冲流包装它，便获得最高的效率,发送的是字符串推荐用字符流，其它数据就用字节流
+            bw.write(jsonstr);//把json字符串写入缓冲区中
+            bw.flush();//刷新缓冲区，把数据发送出去，这步很重要
+            out.close();
+            bw.close();//使用完关闭
+
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {//得到服务端的返回码是否连接成功
+
+                //------------字符流读取服务端返回的数据------------
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String str = null;
+                StringBuffer buffer = new StringBuffer();
+                if ((str = br.readLine()) != null) {//BufferedReader特有功能，一次读取一行数据
+                    buffer.append(str);
+
+
+                    LogUtil.d("1",str+"1");
+                }
+
+                in.close();
+                br.close();
+                return str;
+            }
+
+        } catch (Exception e) {
+
+        } finally {
+            urlConnection.disconnect();//使用完关闭TCP连接，释放资源
+        }
+        return null;
+    }
+
+
+    public String getPraiseNumber(String getUrl, Posto posto) {
+        HttpURLConnection urlConnection = null;
+
+        URL url = null;
+        try {
+
+            url = new URL(getUrl);
+
+            urlConnection = (HttpURLConnection) url.openConnection();//打开http连接
+            urlConnection.setConnectTimeout(3000);//连接的超时时间
+            urlConnection.setUseCaches(false);//不使用缓存
+            //urlConnection.setFollowRedirects(false);是static函数，作用于所有的URLConnection对象。
+            urlConnection.setInstanceFollowRedirects(true);//是成员函数，仅作用于当前函数,设置这个连接是否可以被重定向
+            urlConnection.setReadTimeout(3000);//响应的超时时间
+            urlConnection.setDoInput(true);//设置这个连接是否可以写入数据
+            urlConnection.setDoOutput(true);//设置这个连接是否可以输出数据
+            urlConnection.setRequestMethod("POST");//设置请求的方式
+            urlConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");//设置消息的类型
+            urlConnection.connect();// 连接，从上述至此的配置必须要在connect之前完成，实际上它只是建立了一个与服务器的TCP连接
+            Gson gson = new Gson();
+            String jsonstr = gson.toJson(posto);
+
+            //------------字符流写入数据------------
+            OutputStream out = urlConnection.getOutputStream();//输出流，用来发送请求，http请求实际上直到这个函数里面才正式发送出去
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));//创建字符流对象并用高效缓冲流包装它，便获得最高的效率,发送的是字符串推荐用字符流，其它数据就用字节流
+            bw.write(jsonstr);//把json字符串写入缓冲区中
+            bw.flush();//刷新缓冲区，把数据发送出去，这步很重要
+            out.close();
+            bw.close();//使用完关闭
+
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {//得到服务端的返回码是否连接成功
+                LogUtil.d("1","122222");
+                //------------字符流读取服务端返回的数据------------
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String str = null;
+                StringBuffer buffer = new StringBuffer();
+                if ((str = br.readLine()) != null) {//BufferedReader特有功能，一次读取一行数据
+                    buffer.append(str);
+
+
+                }
+
+                gson = new Gson();
+                String tmp=buffer.toString();
+					/*Type listType = new TypeToken<ArrayList<Book> >(){}.getType();
+					books=gson.fromJson(tmp,listType);*/
+
+
+
+
+
+
+                in.close();
+                br.close();
+                return  tmp;
+            }
+
+        } catch (Exception e) {
+
+        } finally {
+            urlConnection.disconnect();//使用完关闭TCP连接，释放资源
+        }
+        return null;
+    }
+
 }
